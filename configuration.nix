71e29dd2969abe8c66c57c0e5c472effab7c9c27
System administrator <1758075541@qq.com>
2021-01-30 20:57:03 +0800

    init

---
+++ configuration.nix
@@ -0,0 +1,189 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+let
+  unstableTarball =
+    fetchTarball
+      https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz; 
+      # https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nix.binaryCaches = [ 
+        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+  ];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+  users.users.alice.extraGroups = [ "networkmanager" ];
+  # networking.networkmanager.unmanaged = [
+  #   "*" "except:type:wwan" "except:type:gsm"
+  # ];
+
+  #   nmtui
+  # programs.nm-applet.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Shanghai";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.wlp2s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "zh_CN.UTF-8";
+    supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
+    inputMethod.enabled = "fcitx";
+  };
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+  fonts = {
+        enableDefaultFonts = true;
+        fontconfig.enable = true;
+        enableFontDir = true;
+        #fontDir.enable = true;
+        enableGhostscriptFonts = true;
+        fonts = with pkgs; [
+            sarasa-gothic
+            noto-fonts
+            noto-fonts-cjk
+            noto-fonts-emoji
+            wqy_microhei
+            wqy_zenhei
+
+        ];
+  };
+
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  
+
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  #services.shadowsocks.enable = true;
+  #services.v2ray.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.rewine = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = pkgs: {
+    unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+    nixpkgs.config.vivaldi = {
+      proprietaryCodecs = true;
+      enableWideVine = true;
+    };
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget  
+  environment.systemPackages = with pkgs; [
+    wget git zsh fzf
+ 
+    neofetch htop
+    lazygit  ranger
+    oh-my-zsh
+  
+    fcitx fcitx-configtool fcitx-engines.rime fcitx-engines.cloudpinyin
+     
+    vlc obs-studio
+
+    kdeApplications.kdenlive
+    kdeApplications.gwenview
+    kdeApplications.ark
+
+    okular libreoffice wpsoffice typora
+    
+    # text 
+    neovim  nodePackages.coc-git
+    emacs
+    # code
+    gcc gdb clang cmake 
+    qt5.full  
+    python3 lua5_3 ghc 
+    qtcreator
+    jdk jetbrains.idea-community
+
+    vscode postman
+    
+    # network    
+    unstable.v2ray unstable.qv2ray
+    # Browser
+    firefox vivaldi vivaldi-ffmpeg-codecs #vivaldi-widevine
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+  #networking.networkmanager.enable = true;
+  #users.users.alice.extraGroups = [ "networkmanager" ];
+  #networking.networkmanager.unmanaged = [
+   #  "*" "except:type:wwan" "except:type:gsm"
+ # ];
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+}
+
