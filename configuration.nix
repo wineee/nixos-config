823292c61efb36fd1d6ecae269632b2bdac9a5fb
System administrator <1758075541@qq.com>
2021-03-03 15:42:46 +0800

    set vsc and python in files

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,17 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 let
-  extensions = (with pkgs.vscode-extensions; [
-      bbenoist.Nix
-      # ms-python.python
-      # ms-azuretools.vscode-docker
-    ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    {
-      name = "remote-ssh-edit";
-      publisher = "ms-vscode-remote";
-      version = "0.47.2";
-      sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-    }
-  ];
-  vscodium-with-extensions = pkgs.vscode-with-extensions.override {
-    vscode = pkgs.vscodium;
-    vscodeExtensions = extensions;
-  };
   #vivaldi-with-ffmpeg = pkgs.vivaldi.override {
   #  proprietaryCodecs = true;
   #  vivaldi-ffmpeg-codecs = pkgs.vivaldi-ffmpeg-codecs;
   #  enableWidevine = true;
   #  vivaldi-widevine = pkgs.vivaldi-widevine;
   #};
+  python-with-my-packages = pkgs.callPackage ./etc/python.nix {};
+  vscodium-with-extensions = pkgs.callPackage ./etc/vscodium.nix {};
 in
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
       
       ./sys
@@ -65,7 +47,7 @@ in
   environment.systemPackages = with pkgs; [
     alacritty dmenu picom nitrogen xmobar betterlockscreen albert
     libxfs xfsprogs 
-    wget git tmux fzf man
+    wget tmux fzf man
     bpytop
     neofetch htop  
     lazygit  ranger
@@ -76,20 +58,24 @@ in
     gwenview
     ark latte-dock
 
-    okular libreoffice typora #nur.picgo
+    okular libreoffice typora 
     
     # text 
     neovim  
     emacs 
     zeal
+    # for eaf
+    git nodejs wmctrl aria xdotool
+    nodePackages.npm 
+    
     # code
     gcc gdb clang clang-tools cmake ninja 
     qtcreator libsForQt5.qt5.qtdoc libsForQt5.qt5.qtbase libsForQt5.qt5.qttools
-    python3Full # python38Packages.pip
-    lua5_3 ghc nodejs go 
+    python-with-my-packages
+    lua5_3 ghc  go 
     jdk jetbrains.idea-community
 
-    vscode postman vscodium-with-extensions
+    vscode postman
     
     # network    
     v2ray qv2ray
@@ -101,5 +87,7 @@ in
     appimage-run
     # kvm
     qemu qemu_kvm virt-manager iproute
-  ];  
+    vscodium-with-extensions
+  ]; 
 }
+
