4675f8901bd32cfe574bb3d335806591d4db73db
System administrator <1758075541@qq.com>
2021-03-05 01:50:55 +0800

    Add fcitx5-configtool

--- sys/i18n.nix
+++ sys/i18n.nix
@@ -6,7 +6,7 @@
     defaultLocale = "zh_CN.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
     inputMethod.enabled = "fcitx5";
-    inputMethod.fcitx5.addons  = [ pkgs.fcitx5-rime ];
+    inputMethod.fcitx5.addons  = [ pkgs.fcitx5-rime pkgs.fcitx5-configtool ];
   };
 
 }
