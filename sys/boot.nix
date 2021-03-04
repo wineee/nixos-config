333066c1beb618321a692cce5c7dc1f50adae5ac
System administrator <1758075541@qq.com>
2021-03-05 03:11:26 +0800

    systemd-boot -> grub2

--- sys/boot.nix
+++ sys/boot.nix
@@ -1,9 +1,22 @@
 { ... }:
 
 {
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Do not Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = false;
+  
+  # Use GRand Unified Bootloader
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      version = 2;
+      device = "nodev";
+      useOSProber = true;
+      efiSupport = true;
+    };
+  };
+  
+  # boot.plymouth.enable = true;
 
   boot.extraModprobeConfig = ''
     options kvm_intel nested=1
