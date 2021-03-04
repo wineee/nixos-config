a175807ed857d761d3288a1c6fb73b8029337266
System administrator <1758075541@qq.com>
2021-03-05 03:39:59 +0800

    set users

--- sys/users.nix
+++ sys/users.nix
@@ -1,10 +1,25 @@
 {pkgs, ... }:
 {
- # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.rewine = {
     isNormalUser = true;
-    useDefaultShell = true;
-    extraGroups = [ "wheel" "networkmanager" "libvirtd"]; # Enable ‘sudo’ for the u>
+    extraGroups = [ "wheel" "networkmanager" "libvirtd"]; 
   };
   users.defaultUserShell = pkgs.zsh;
+
+  security.polkit = {
+    enable = true;
+    extraConfig = ''
+       /* Log authorization checks. */
+      polkit.addRule(function(action, subject) {
+        polkit.log("user " +  subject.user + " is attempting action " + action.id + " from PID " + subject.pid);
+      });
+  
+      /* Allow any local user to do anything (dangerous!). */
+      polkit.addRule(function(action, subject) {
+        if (subject.local) return "yes";
+      });
+
+    '';
+  };
+
 }
