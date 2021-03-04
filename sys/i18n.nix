8541376381a74d0a7c55b2a59f293d4d4b01d72a
System administrator <1758075541@qq.com>
2021-03-05 01:44:26 +0800

    Upgrade fcitx -> fcitx 5

--- sys/i18n.nix
+++ sys/i18n.nix
@@ -5,8 +5,8 @@
   i18n = {
     defaultLocale = "zh_CN.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
-    inputMethod.enabled = "fcitx";
-    inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ rime cloudpinyin ];
+    inputMethod.enabled = "fcitx5";
+    inputMethod.fcitx5.addons  = [ pkgs.fcitx5-rime ];
   };
 
 }
