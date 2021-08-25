8df9785b86a5b088590d4cd5bd0e8b520ec5ffa6
System administrator <1758075541@qq.com>
2021-08-25 17:13:27 +0800

    enable kde5

--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,5 @@
 { config, pkgs, ... }:
 let
-  #vivaldi-with-ffmpeg = pkgs.vivaldi.override {
-  #  proprietaryCodecs = true;
-  #  vivaldi-ffmpeg-codecs = pkgs.vivaldi-ffmpeg-codecs;
-  #  enableWidevine = true;
-  #  vivaldi-widevine = pkgs.vivaldi-widevine;
-  #};
   python-with-my-packages = pkgs.callPackage ./etc/python.nix {};
   vscodium-with-extensions = pkgs.callPackage ./etc/vscodium.nix {};
 in
@@ -34,26 +28,13 @@ in
 
   nixpkgs.config = {
     allowUnfree = true;
-<<<<<<< HEAD
     vivaldi = {
       proprietaryCodecs = true;
       enableWideVine = true;
     };
-=======
->>>>>>> 7f5db9b7f084e2873c5b077b77e4a2677f8c6e9a
-    packageOverrides = pkgs: {
-      nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-        inherit pkgs;
-      };
-      vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; }; 
-    };
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; }; 
   };  
 
-<<<<<<< HEAD
-  #nixpkgs.config.vivaldi.proprietaryCodecs = true;
-=======
-  nixpkgs.config.vivaldi.proprietaryCodecs = true;
->>>>>>> 7f5db9b7f084e2873c5b077b77e4a2677f8c6e9a
 
   environment.systemPackages = with pkgs; [
     alacritty dmenu picom nitrogen xmobar
@@ -73,45 +54,32 @@ in
     ark latte-dock
 
     okular libreoffice typora 
-<<<<<<< HEAD
     # modern unix
     tldr exa fd  
     # text 
     neovim  
     emacs 
     yi
-=======
     
     # text 
     neovim  
     emacs 
     
->>>>>>> 7f5db9b7f084e2873c5b077b77e4a2677f8c6e9a
     # for eaf
     git nodejs wmctrl aria xdotool
     
     # code
     gcc gdb clang clang-tools cmake ninja 
-<<<<<<< HEAD
     #qtcreator 
     python-with-my-packages
     #lua5_3 ghc  go 
     #jdk jetbrains.idea-community
-=======
-    qtcreator 
-    python-with-my-packages
-    lua5_3 ghc  go 
-    jdk jetbrains.idea-community
->>>>>>> 7f5db9b7f084e2873c5b077b77e4a2677f8c6e9a
 
     vscode postman
     
     # network    
     v2ray qv2ray
-<<<<<<< HEAD
     clash 
-=======
->>>>>>> 7f5db9b7f084e2873c5b077b77e4a2677f8c6e9a
     # Browser
     firefox 
     vivaldi
--- sys/desktop.nix
+++ sys/desktop.nix
@@ -1,16 +1,14 @@
 { pkgs, lib, ... }:
 
 {
-   # Enable the Plasma 5 Desktop Environment.
   services.xserver.enable = true;
-  #services.xserver.displayManager.sddm.enable = true;
-  #services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.pantheon.enable = true;
-  services.xserver.displayManager.lightdm.greeters.pantheon.enable = true;
-  
-  #services.xserver.displayManager.defaultSession = "none+xmonad";
 
+  services.xserver.desktopManager.pantheon.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;  
+  #services.xserver.displayManager.defaultSession = "none+xmonad";
+  
+  
   services.xserver.windowManager.xmonad = {
     enable = true;
     enableContribAndExtras = true;
