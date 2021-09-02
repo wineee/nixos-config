fa7047347e56067c295906d58eef0f51a499eb42
System administrator <1758075541@qq.com>
2021-09-03 02:09:38 +0800

    better default font

--- sys/fonts.nix
+++ sys/fonts.nix
@@ -4,13 +4,14 @@
   fonts = {
         enableDefaultFonts = true;
         fontconfig = {
- 		enable = true;
-		defaultFonts.emoji = [
- 			 "Noto Color Emoji"
-		];
+ 	    enable = true;
+	    defaultFonts.emoji = [ "Noto Color Emoji" ];
+            defaultFonts.monospace = [ "Hack" "Sarasa Mono SC" ];
+            #defaultFonts.sansSerif = [ "Inter" "Liberation Sans" "Soruce Han Sans SC" ];
+            #defaultFonts.serif = [ "Liberation Serif" "Source Han Serif SC" ];
                 
 	};
-        #enableFontDir = true;
+        
         fontDir.enable = true;
         enableGhostscriptFonts = true;
         fonts = with pkgs; [
@@ -20,7 +21,8 @@
             noto-fonts-emoji
             wqy_microhei
             wqy_zenhei
-	    nerdfonts
+	    nerdfonts # 图标字体
+            symbola  # 特殊字符
         ];
   };
 }
