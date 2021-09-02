5e02f1cd5caa9d8f72000e0b39bd1e5b7f91ac54
System administrator <1758075541@qq.com>
2021-09-03 02:22:47 +0800

    enable firewall

--- sys/network.nix
+++ sys/network.nix
@@ -11,10 +11,6 @@
   # use nmcil/nmtui to manager network 
   programs.nm-applet.enable = true;
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
   networking.interfaces.wlp2s0.useDHCP = true;
 
   # Configure network proxy if necessary
@@ -23,10 +19,9 @@
   networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 80 ];
+  networking.firewall.allowedUDPPorts = [ ];
 
   networking.extraHosts =
   ''
