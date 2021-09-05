18b403f41e783f3d72540bad08975166a0d65255
System administrator <1758075541@qq.com>
2021-09-05 13:07:56 +0800

    set alias for exa

--- etc/alias.nix
+++ etc/alias.nix
@@ -2,14 +2,17 @@
 
 {
   environment.shellAliases = {
-    l = "exa";
-    ll = "exa -l";
+    ls = "exa --icons";
+    l = "exa -l";
+    ll = "exa -al";
     la = "exa -a";
     tree = "exa --tree";
+
     c = "clear";
     r = "ranger";
     vi = "nvim";
     ems = "emacs -nw";
+    
     nix-zsh = "nix-shell --run zsh";
     buildos = "sudo nixos-rebuild switch";
   };
