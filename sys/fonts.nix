7e07c04b59916f211e1b644f8d1ae305f5aa935d
System administrator <1758075541@qq.com>
2021-09-16 22:51:22 +0800

    better font config

--- sys/fonts.nix
+++ sys/fonts.nix
@@ -4,25 +4,31 @@
   fonts = {
         enableDefaultFonts = true;
         fontconfig = {
- 	    enable = true;
-	    defaultFonts.emoji = [ "Noto Color Emoji" ];
-            defaultFonts.monospace = [ "Hack" "Sarasa Mono SC" ];
-            #defaultFonts.sansSerif = [ "Inter" "Liberation Sans" "Soruce Han Sans SC" ];
-            #defaultFonts.serif = [ "Liberation Serif" "Source Han Serif SC" ];
-                
+          enable = true;
+          dpi = 0;
+          hinting = {
+            enable = true;
+            autohint = false;
+            # hintstyle = "hintslight(10px,12px)";
+          };
+	  defaultFonts.emoji = [ "Noto Color Emoji" ];
+          defaultFonts.monospace = [ "Hack" "Sarasa Mono SC" ];
+          defaultFonts.sansSerif = [ "DejaVu Sans" ];
+          defaultFonts.serif = [ "DejaVu Serif" "Source Han Serif SC" ];                
 	};
         
         fontDir.enable = true;
         enableGhostscriptFonts = true;
         fonts = with pkgs; [
-            sarasa-gothic
-            noto-fonts
-            noto-fonts-cjk
-            noto-fonts-emoji
-            wqy_microhei
-            wqy_zenhei
-	    nerdfonts # 图标字体
-            symbola  # 特殊字符
+          sarasa-gothic
+          noto-fonts
+          noto-fonts-cjk
+          noto-fonts-emoji
+          wqy_microhei
+          wqy_zenhei
+          nerdfonts # 图标字体
+          symbola  # 特殊字符
+          jetbrains-mono
         ];
   };
 }
