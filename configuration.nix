5db01074b7e68a983fb0f9483297a4828cfa7991
System administrator <1758075541@qq.com>
2021-11-01 21:45:41 +0800

    dump emacs-overlay

--- configuration.nix
+++ configuration.nix
@@ -68,7 +68,7 @@ in
     (import (builtins.fetchGit {
       url = "https://github.com/nix-community/emacs-overlay.git";
       ref = "master";
-      rev = "bfc8f6edcb7bcf3cf24e4a7199b3f6fed96aaecf"; # change the revision
+      rev = "15ecda897ac72e1d3812306847beb5b7b981fdd6"; # change the revision
     }))
   ];
 
--- etc/alias.nix
+++ etc/alias.nix
@@ -8,10 +8,11 @@
     la = "exa -a";
     tree = "exa --tree";
 
+    ":q" = "exit";
     c = "clear";
     r = "ranger";
     ems = "emacs -nw";
-    
+  
     nix-zsh = "nix-shell --run zsh";
     buildos = "sudo nixos-rebuild switch";
   };
