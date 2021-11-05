b0af090bc62b9700114ad90c38329c4f4a95cda5
System administrator <1758075541@qq.com>
2021-11-05 17:29:56 +0800

    fix nvidia driver

--- sys/gpu.nix
+++ sys/gpu.nix
@@ -1,8 +1,23 @@
 { lib, config, pkgs, ... }:
 { 
-  services.xserver.videoDrivers = [ "vesa" "modesetting" ]; #"nvidia"
+  services.xserver.videoDrivers = [ "nvidia" "vesa" "modesetting" ]; #"nvidia"
   services.xserver.useGlamor = true;
   # hardware.bumblebee.enable = true;
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+      prime = {
+        offload.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+
+    opengl = {
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+  };
 
   # Enable OpenCL support for Intel Gen8 and later GPUs
   nixpkgs.config.packageOverrides = pkgs: {
