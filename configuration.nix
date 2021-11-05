f2ff434d6b41a8fd5fec99f0693a6d0ed032241a
System administrator <1758075541@qq.com>
2021-11-06 03:07:30 +0800

    neovim config

--- configuration.nix
+++ configuration.nix
@@ -58,7 +58,10 @@ in
           # manually loadable by calling `:packadd $plugin-name`
           opt = [  ];
         };
-  };
+        customRC = ''
+          set whichwrap+=<,>,[,],h,l
+        '';
+      };
 
 
     };
