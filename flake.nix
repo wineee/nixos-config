a94d0b953f84b505c7e3784312493a3dc27e0b1d
System administrator <1758075541@qq.com>
2022-01-22 14:50:50 +0800

    update

--- flake.nix
+++ flake.nix
@@ -3,10 +3,39 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";                
 
-  outputs = { self, nixpkgs, ...}@inputs: {
-    nixosConfigurations.nixos =  nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+  inputs.nixos-cn = {
+    url = "github:nixos-cn/flakes";
+    # 强制 nixos-cn 和该 flake 使用相同版本的 nixpkgs
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, nixos-cn, ...}@inputs:
+    let  system = "x86_64-linux";
+    in {
+      nixosConfigurations.nixos =  nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ 
+          ./configuration.nix
+           ({ ... }: {
+            # 使用 nixos-cn flake 提供的包
+            environment.systemPackages =
+              [ nixos-cn.legacyPackages.${system}.netease-cloud-music ];
+            # 使用 nixos-cn 的 binary cache
+            nix.binaryCaches = [
+              "https://nixos-cn.cachix.org"
+            ];
+            nix.binaryCachePublicKeys = [ "nixos-cn.cachix.org-1:L0jEaL6w7kwQOPlLoCR3ADx+E3Q8SEFEcB9Jaibl0Xg=" ];
+
+            imports = [
+              # 将nixos-cn flake提供的registry添加到全局registry列表中
+              # 可在`nixos-rebuild switch`之后通过`nix registry list`查看
+              nixos-cn.nixosModules.nixos-cn-registries
+
+              # 引入nixos-cn flake提供的NixOS模块
+              nixos-cn.nixosModules.nixos-cn
+            ];
+          })
+        ];
         specialArgs = { inherit inputs; };
       };
     };
