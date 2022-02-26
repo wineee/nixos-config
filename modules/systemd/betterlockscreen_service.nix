63eed9679465958c53564d453d92d56d389ad2ea
System administrator <1758075541@qq.com>
2022-02-27 01:25:38 +0800

    mv: systemd into modules

---
+++ modules/systemd/betterlockscreen_service.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+{
+  systemd.services."betterlockscreen@rewine" = {
+    enable = false;
+    path = [ pkgs.betterlockscreen pkgs.coreutils ];
+    before = [ "sleep.target" "suspend.target" ];
+    wantedBy = [ "sleep.target" "suspend.target" ];
+    description = "Lock screen when going to sleep/suspend";
+    environment = {
+      DISPLAY=":0";
+    };
+    serviceConfig = {
+      Type = "simple";
+      User = "%I";
+      WorkingDirectory="/home/rewine";
+      ExecStart = "${pkgs.betterlockscreen}/bin/betterlockscreen --lock";
+      TimeoutSec = "infinity"; # 0
+      ExecStartPost = "${pkgs.coreutils}/bin/sleep 1";
+    };
+  };
+}
