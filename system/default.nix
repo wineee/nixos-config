b8d4166cf2d02e51454499b9ffbc538992501e10
System administrator <1758075541@qq.com>
2022-02-27 02:00:34 +0800

    mv: base.nix

--- system/default.nix
+++ system/default.nix
@@ -3,6 +3,7 @@
 {
   imports =
     [
+      ./base.nix
       ./boot.nix
       ./fonts.nix
       ./gpu.nix
@@ -14,46 +15,4 @@
       #./virtualisation.nix
       #./auto.nix
     ];
-  # Set your time zone.
-  time.timeZone = "Asia/Shanghai";
-
-  #networking.hostId = "71452b6e"; # for zfs pool
-  boot.supportedFilesystems = ["xfs" "btrfs" ];
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-  
-  # Enabling Bluetooth support
-  hardware.bluetooth.enable = true;
-
-  # set console font
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
-
-  # Enable Flatpak
-  xdg.portal.enable = true;
-  services.flatpak.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
- 
- # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-  
-  system.stateVersion = "22.05"; 
 }
