013eb4faf8e8e14324fbe0328c16479a013bc80f
System administrator <1758075541@qq.com>
2022-03-01 00:17:01 +0800

    flake: import nur

--- flake.nix
+++ flake.nix
@@ -7,24 +7,30 @@
       url = "github:nixos-cn/flakes";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nur.url = github:nix-community/NUR;
   };
 
-  outputs = { self, nixpkgs, nixos-cn, ...}@inputs:
+  outputs = { self, nixpkgs, nixos-cn, nur, ...}@inputs:
     let system = "x86_64-linux";
     in {
       nixosConfigurations.nixos =  nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [ 
-          ./configuration.nix
-           ({ ... }: {
-            environment.systemPackages =
-              [ nixos-cn.legacyPackages.${system}.netease-cloud-music ];
+          { nixpkgs.overlays = [ nur.overlay ]; }
+        
+          ({ ... }: {
+            environment.systemPackages = [
+              nixos-cn.legacyPackages.${system}.netease-cloud-music 
+            #  nur.repos.0x4A6F.nixpkgs-check
+            ];
               
             imports = [
               nixos-cn.nixosModules.nixos-cn-registries
               nixos-cn.nixosModules.nixos-cn
             ];
           })
+
+          ./configuration.nix
         ];
         specialArgs = { inherit inputs; };
       };
