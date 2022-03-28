10b4315f6787a6c9fd25882c66f98fdca18e4918
System administrator <1758075541@qq.com>
2022-03-28 23:22:13 +0800

    use fcitx5

--- system/i18n.nix
+++ system/i18n.nix
@@ -5,10 +5,10 @@
     defaultLocale = "zh_CN.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
     inputMethod = {
-      enabled = "ibus"; #"fcitx5";
+      # enabled = "ibus"; 
+      # ibus.engines = with pkgs.ibus-engines; [ rime uniemoji libpinyin ];
 
-      ibus.engines = with pkgs.ibus-engines; [ rime uniemoji libpinyin ];
-
+      enabled = "fcitx5";
       fcitx5.addons = with pkgs; [
         fcitx5-rime
         fcitx5-configtool
