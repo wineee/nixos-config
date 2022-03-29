b7aae980def7804377b7b4d7943163556cdb7eaa
System administrator <1758075541@qq.com>
2022-03-29 15:06:06 +0800

    update

--- system/desktop.nix
+++ system/desktop.nix
@@ -8,25 +8,35 @@
   };
   #programs.qt5ct.enable = true;
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.defaultSession = "none+xmonad";
-  services.xserver.desktopManager.plasma5.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+
+      displayManager = {
+        sddm.enable = true;
+        defaultSession = "none+xmonad";
+      };
+
+      ### kde plasma
+      desktopManager.plasma5.enable = true;
+
+      ### xmonad
+      windowManager.xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+        # config = ../.xmonad/xmonad.hs;
+        extraPackages = haskellPackages: [
+          #haskellPackages.dmenu
+          haskellPackages.xmonad
+          haskellPackages.xmonad-contrib
+          haskellPackages.monad-logger
+          haskellPackages.xmonad-extras
+          haskellPackages.xmonad-wallpaper
+        ];
+      };
+    };
+  };
 
   # a DBus service that provides power management support to applications
   services.upower.enable = true;
-
-  services.xserver.windowManager.xmonad = {
-    enable = true;
-    enableContribAndExtras = true;
-    # config = ../.xmonad/xmonad.hs;
-    extraPackages = haskellPackages: [
-      #haskellPackages.dmenu
-      haskellPackages.xmonad
-      haskellPackages.xmonad-contrib
-      haskellPackages.monad-logger
-      haskellPackages.xmonad-extras
-      haskellPackages.xmonad-wallpaper
-    ];
-  };
 }
