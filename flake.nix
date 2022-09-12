56ac4f53028308c0c53a66d0526a31abadd22f9e
System administrator <1758075541@qq.com>
2022-09-12 17:14:51 +0800

    remove unused input

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    #nixpkgs-master.url = "nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils-plus = {
       url = "github:gytis-ivaskevicius/flake-utils-plus";
@@ -17,13 +16,6 @@
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    berberman = {
-      url = "github:berberman/flakes";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    rew = {
-      url = "github:wineee/rew-flakes";
-    };
     nix-dram = {
       url = "github:dramforever/nix-dram";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -32,20 +24,15 @@
   };
 
   outputs = { self, nixpkgs,  flake-utils, flake-utils-plus,
-              nixos-cn, nur, rew, berberman, nix-dram, ... }@inputs:
+              nixos-cn, nur, nix-dram, ... }@inputs:
     let
       system = "x86_64-linux";
-
-      #pkgs' = import nixpkgs-master {
-      #  inherit system;
-      #  config.allowUnfree = true;
-      #};
     in
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
-          { nixpkgs.overlays = [ nur.overlay berberman.overlay nix-dram.overlay ]; }
+          { nixpkgs.overlays = [ nur.overlay nix-dram.overlay ]; }
 
           { nixpkgs.config.permittedInsecurePackages = [ "electron-9.4.4" ]; }
 
