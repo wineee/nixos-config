b9c0c22b8ec8b53fe854a650b9792eac3094c743
rewine <lhongxu@outlook.com>
2022-12-23 14:20:44 +0800

    add cache.garnix.io channel

--- nixconfig.nix
+++ nixconfig.nix
@@ -14,9 +14,11 @@
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
         "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://rewine.cachix.org"
+        "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "rewine.cachix.org-1:aOIg9PvwuSefg59gVXXxGIInHQI9fMpskdyya2xO+7I="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
       trusted-users = [ "root" "rewine" ];
     };
