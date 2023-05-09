92b72abd66eaf3b8df33e68b5cfca45ef1f08cbd
System administrator <1758075541@qq.com>
2023-05-09 14:26:13 +0800

    use nouveau

--- system/gpu.nix
+++ system/gpu.nix
@@ -1,22 +1,17 @@
 { lib, config, pkgs, ... }:
 {
-  services.xserver.videoDrivers = [ "nvidia" "vesa" "modesetting" ]; #"nvidia"
+  services.xserver.videoDrivers = [ "nouveau" "vesa" "modesetting" ]; #"nvidia"
   # hardware.bumblebee.enable = true;
   hardware = {
-    nvidia = {
-      modesetting.enable = true;
-      prime = {
+    #nvidia = {
+    #  modesetting.enable = true;
+    #  prime = {
         # nix-shell -p pciutils --run lspci
-        offload.enable = true;
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
-
-    opengl = {
-      driSupport = true;
-      driSupport32Bit = true;
-    };
+    #    offload.enable = true;
+    #    intelBusId = "PCI:0:2:0";
+    #    nvidiaBusId = "PCI:1:0:0";
+    #  };
+    #};
   };
 
   # Enable OpenCL support for Intel Gen8 and later GPUs
@@ -26,6 +21,8 @@
 
   hardware.opengl = {
     enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firef>
