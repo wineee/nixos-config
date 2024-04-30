f9a1d8bf3f053a67c21ec0fd8c0699f366fdac9d
rewine <luhongxu@deepin.org>
2024-05-01 01:06:26 +0800

    rename services.libinput.enable

--- system/base.nix
+++ system/base.nix
@@ -28,7 +28,7 @@
 
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
 
   # Enabling Bluetooth support
   hardware.bluetooth.enable = true;
