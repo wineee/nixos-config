061baeb4ecfcf67a06c098ace6f5183163b3bbf4
rewine <luhongxu@deepin.org>
2024-09-19 14:29:21 +0800

    update

--- desktop/deepin.nix
+++ desktop/deepin.nix
@@ -4,8 +4,6 @@
   services = {
     xserver = {
       enable = true;
-      #displayManager.lightdm.enable = true;
-      #desktopManager.deepin-unstable.enable = true;
       desktopManager.deepin.enable = true;
     };
   };
