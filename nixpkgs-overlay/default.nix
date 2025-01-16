12f273d331a190ed8c8b7b521348609ac5fd0d3a
rewine <luhongxu@deepin.org>
2025-01-16 14:31:12 +0800

    Remove overlay for wayfire

--- nixpkgs-overlay/default.nix
+++ nixpkgs-overlay/default.nix
@@ -1,14 +1,3 @@
 self: super:
 rec {
-  #wayfire = self.callPackage pkgs/applications/window-managers/wayfire/default.nix {
-  #  wlroots = super.wlroots_0_16;
-  #};
-  #wf-config = self.callPackage pkgs/applications/window-managers/wayfire/wf-config.nix { };
-
-  #wayfirePlugins = super.recurseIntoAttrs (
-  #  self.callPackage pkgs/applications/window-managers/wayfire/plugins.nix { }
-  #);
-  #wayfire-with-plugins = self.callPackage pkgs/applications/window-managers/wayfire/wrapper.nix {
-  #  plugins = with wayfirePlugins; [ wcm wf-shell wayfire-plugins-extra ];
-  #};
 }
