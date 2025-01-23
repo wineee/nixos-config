645bd5bcabbe61b1cf415d08480ca8f3c4c088c4
rewine <luhongxu@deepin.org>
2025-01-23 16:54:27 +0800

    Fix build for lucenepp

--- nixpkgs-overlay/default.nix
+++ nixpkgs-overlay/default.nix
@@ -1,3 +1,21 @@
 self: super:
 rec {
+  lucenepp = (super.lucenepp.override {
+    boost = super.boost186;
+  }).overrideAttrs ( old : {
+    patches = old.patches ++ [
+      (self.fetchpatch {
+        name = "migrate-to-boost_asio_io_context.patch";
+        url = "https://github.com/luceneplusplus/LucenePlusPlus/commit/e6a376836e5c891577eae6369263152106b9bc02.patch";
+        hash = "sha256-0mdVvrS0nTxSJXRzVdx2Zb/vm9aVxGfARG/QliRx7tA=";
+      })
+    ];
+    doCheck = false;
+  });
+  dde-grand-search = super.dde-grand-search.override {
+    boost = super.boost186;
+  };
+  dde-file-manager = super.dde-file-manager.override {
+    boost = super.boost186;
+  };
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,7 +11,7 @@ let
     ninja
     qtcreator
     clang
-    cargo rustfmt clippy
+    cargo rustfmt clippy rust-analyzer
     cppcheck
     # clazy
     go gopls
