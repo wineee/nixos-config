058295b71ea776a06143a119b8dad89f2de60e5e
rewine <luhongxu@deepin.org>
2025-08-14 16:02:45 +0800

    Update

--- system/i18n.nix
+++ system/i18n.nix
@@ -6,7 +6,7 @@
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
     inputMethod = {
       enable = true;
-      type = "ibus"; #"fcitx5";
+      type = "fcitx5";
       fcitx5.addons = with pkgs; [
         fcitx5-rime
         fcitx5-configtool
