8c0a9146d436f1b950fd2c6ed696801d84d22f3c
rewine <luhongxu@deepin.org>
2025-09-17 11:07:01 +0800

    chore: enable nix-index

--- hosts/default.nix
+++ hosts/default.nix
@@ -7,23 +7,14 @@ let
     ../nixconfig.nix
     ../nixpkgsconfig.nix
     ../users.nix
-    {
-      environment.systemPackages = [
-        #inputs.agenix.defaultPackage.x86_64-linux
-      ];
-      systemd.packages = [
-      ];
-    }
-
   ] ++ (with inputs; [
-    # agenix.nixosModule
     home-manager.nixosModules.home-manager
     linglong-flake.nixosModules.linyaps
-    #inputs.nix-index-database.nixosModules.nix-index
+    inputs.nix-index-database.nixosModules.nix-index
     { 
-      #programs.nix-index-database.comma.enable = true;
-      #programs.nix-index.enableBashIntegration = false;
-      #programs.nix-index.enableZshIntegration = false;
+      programs.nix-index-database.comma.enable = true;
+      programs.nix-index.enableBashIntegration = false;
+      programs.nix-index.enableZshIntegration = false;
 
       services.linyaps.enable = true;
     }
