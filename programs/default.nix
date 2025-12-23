33fee7dc99b9fbf9fb56ad93092768aaf6cf3ef1
rewine <luhongxu@deepin.org>
2025-12-23 20:02:36 +0800

    Update

--- programs/default.nix
+++ programs/default.nix
@@ -7,9 +7,4 @@
 
   #programs.nix-ld.enable = true;
   programs.command-not-found.enable = true;
-  programs.sway.enable = true;
-
-  services.clamav.scanner.enable = true;
-  services.clamav.daemon.enable = true;
-  services.clamav.updater.enable = true;
 }
